e9a620cd4d8428a633fa698e1e06f6a0c7dbe13f
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2022-07-28 00:01:23 +0300

    0.1.0 - Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>lil-m00ha blog</title>
+                <link rel="icon" href="/parrot.png"/>
+            </Head>
+            
+            <main></main>
+        </div>
+    )
+}
