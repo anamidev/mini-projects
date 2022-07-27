385ce2537de0fe2784b0a4f420863ac16c4d9245
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2022-07-28 00:32:50 +0300

    0.2.0 - Layout * Added layout for "Home" page * Added test "First post" page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -8,7 +9,32 @@ export default function Home() {
                 <link rel="icon" href="/parrot.png"/>
             </Head>
             
-            <main></main>
+            <main>
+                <header>
+                    <h2>Welcome to the BLOG!</h2>
+                    <p>Different kind of articles can be found here</p>
+                </header>
+                
+                <section>
+                    <Link href="/posts/first-post">
+                        <a>
+                            <article>
+                                <h3>First post header</h3>
+                                <p>First post description</p>
+                            </article>
+                        </a>
+                    </Link>
+                    
+                    <article>
+                        <h3>Second post header</h3>
+                        <p>Second post description</p>
+                    </article>
+                </section>
+                
+                <footer>
+                    footer info
+                </footer>
+            </main>
         </div>
     )
 }
---
+++ pages/posts/first-post.js
@@ -0,0 +1,7 @@
+export default function FirstPost() {
+    return (
+        <div>
+            <p>first post</p>
+        </div>
+    )
+}
