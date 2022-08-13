96ea9fcb5e3ea3d870a77e909fc9d32ce14f2077
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2022-08-14 00:28:12 +0300

    1.3.0 - 'Drawing board' page New features * Added new page 'Drawing board' * Added link to the new page

---
+++ pages/projects/drawing-board.js
@@ -0,0 +1,88 @@
+import Layout from "../../components/Layout/Layout";
+import {useEffect, useRef, useState} from "react";
+import styles from "../../styles/drawing-board.module.css";
+
+export default function DrawingBoard() {
+    const canvas = useRef();
+    const [canvasSize, setCanvasSize] = useState({
+        width: 800,
+        height: 800,
+    });
+    
+    let ctx;
+    let isDrawing = false;
+    let hue = 0;
+    let points = [];
+    
+    const updateHue = () => {
+        if (hue >= 360) {
+            hue = 0;
+        } else {
+            hue += 1;
+        }
+    };
+    
+    const startDraw = (e) => {
+        points = [];
+        isDrawing = true;
+        points.push({x: e.clientX, y: e.clientY});
+    };
+    const endDraw = () => {
+        isDrawing = false;
+        points.length = 0;
+    };
+    const draw = (e) => {
+        if (!isDrawing) return;
+        
+        points.push({x: e.clientX, y: e.clientY});
+        
+        ctx.strokeStyle = `hsla(${hue}, 100%, 50%, 0.25)`;
+        ctx.beginPath();
+        ctx.moveTo(points[points.length - 2].x, points[points.length - 2].y);
+        ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
+        ctx.stroke();
+        
+        for (let i = 0, len = points.length; i < len; i++) {
+            let dx = points[i].x - points[points.length-1].x;
+            let dy = points[i].y - points[points.length-1].y;
+            let d = dx * dx + dy * dy;
+            
+            if (d < 20000 && Math.random() > 0.5) {
+                ctx.beginPath();
+                ctx.moveTo( points[points.length-1].x + (dx * 0.2), points[points.length-1].y + (dy * 0.2));
+                ctx.lineTo( points[i].x - (dx * 0.2), points[i].y - (dy * 0.2));
+                ctx.stroke();
+            }
+        }
+        updateHue();
+    }
+    
+    useEffect(() => {
+        setCanvasSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        })
+    }, [])
+    useEffect(() => {
+        canvas.current.width = canvasSize.width;
+        canvas.current.height = canvasSize.height;
+        
+        ctx = canvas.current.getContext('2d');
+        ctx.lineWidth = 1;
+        ctx.lineJoin = ctx.lineCap = 'round';
+    }, [canvasSize])
+    
+    return (
+        <Layout title={'Drawing Board'}>
+            <section className={styles.container}>
+                <canvas ref={canvas}
+                        onMouseMove={draw}
+                        onMouseDown={startDraw}
+                        onMouseUp={endDraw}
+                        width={canvasSize.width}
+                        height={canvasSize.height}>
+                </canvas>
+            </section>
+        </Layout>
+    )
+}
