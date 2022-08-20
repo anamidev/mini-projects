124be23b83b4cea467c491fdca1524adf00372f9
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2022-08-20 13:11:44 +0300

    1.4.1 - Fixing mobile scaling Adjustments * Added <meta> tag to Layout component to fix scaling on mobile devices

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,6 +7,7 @@ export default function Layout({ title, children }) {
             <Head>
                 <title>{title ? defaultTitle + ' - ' + title : defaultTitle}</title>
                 <link rel="icon" href="/parrot.png"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
             </Head>
             
             <main>
